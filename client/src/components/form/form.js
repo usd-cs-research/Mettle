1ef8570c1618808aac9f7f710c13ec7c4659b154
Shrey-113 <shreyanshngp11@gmail.com>
2023-08-03 10:25:07 +0530

    axios fi

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams} from 'react-router-dom';
-import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Form.css';
 import Popup from './popup';
 
 const Form = () => {
 	const navigate = useNavigate();
 	const [questionId, setQuestionId] = useState(null);
-	
 
 	const initialMiniQuestion = {
 		question: '',
@@ -152,9 +150,6 @@ const Form = () => {
 	const [imageURL, setImageURL] = useState(null);
 	const [pdfURL, setPDFURL] = useState(null);
 
-	
-
-
 	const [showPopup, setShowPopup] = useState(false);
 
 	const handleMainQuestionChange = (e) => {
@@ -163,18 +158,17 @@ const Form = () => {
 
 	const [imagePreview, setImagePreview] = useState(null);
 
-  // Update image preview when a new image is selected
-  const [selectedImageURL, setSelectedImageURL] = useState('');
+	// Update image preview when a new image is selected
+	const [selectedImageURL, setSelectedImageURL] = useState('');
 
-  // Modify the handleImageChange function
+	// Modify the handleImageChange function
 	const handleImageChange = (e) => {
 		const file = e.target.files[0];
 		setImage(file); // Store the image Blob in state
-	
+
 		// Create the URL and set it in the state
 		setImageURL(URL.createObjectURL(file));
 	};
-	
 
 	const handlePdfChange = (e) => {
 		const file = e.target.files[0];
@@ -244,57 +238,63 @@ const Form = () => {
 		};
 		setSubquestions(updatedSubquestions);
 	};
-	
 
-  // Other state variables and functions...
+	// Other state variables and functions...
 
+	// Other state variables and functions...
 
-		// Other state variables and functions...
-	
-		const baseUrl = 'http://localhost:5000/';
-	
-		const fetchFormData = async () => {
-			try {
-				const response = await fetch(
-					`http://localhost:5000/question?questionId=${paramQuestionId}`,
-					{
-						headers: {
-							Authorization: `Bearer ${localStorage.getItem('token')}`,
-						},
-					}
-				);
-				const data = await response.json();
-		console.log(data);
-				if (data.question) {
-					const questionData = data.question;
-					setMainQuestion(questionData.question);
-					const imageSrc = questionData.image.startsWith('media/images/')
+	const baseUrl = 'http://localhost:5000/';
+
+	const fetchFormData = async () => {
+		try {
+			const response = await fetch(
+				`http://localhost:5000/question?questionId=${paramQuestionId}`,
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem(
+							'token',
+						)}`,
+					},
+				},
+			);
+			const data = await response.json();
+			console.log(data);
+			if (data.question) {
+				const questionData = data.question;
+				setMainQuestion(questionData.question);
+				const imageSrc = questionData.image.startsWith('media/images/')
 					? baseUrl + questionData.image
 					: baseUrl + 'media/images/' + questionData.image;
-					const pdfSrc = baseUrl + questionData.info;
-					
-					// Set the image and pdf URLs to state
-					setSelectedImageURL(imageSrc);
-					setPdf(pdfSrc);
-					setQuestionId(paramQuestionId);
-					
-					// Set the image and pdf URLs by concatenating the media paths to the base URL
-					// ...
-		
-					// Map the subquestions data to the state structure
-					if (questionData.subQuestions && questionData.subQuestions.length > 0) {
-						const mappedSubQuestions = questionData.subQuestions.map((subQuestion) => {
+				const pdfSrc = baseUrl + questionData.info;
+
+				// Set the image and pdf URLs to state
+				setSelectedImageURL(imageSrc);
+				setPdf(pdfSrc);
+				setQuestionId(paramQuestionId);
+
+				// Set the image and pdf URLs by concatenating the media paths to the base URL
+				// ...
+
+				// Map the subquestions data to the state structure
+				if (
+					questionData.subQuestions &&
+					questionData.subQuestions.length > 0
+				) {
+					const mappedSubQuestions = questionData.subQuestions.map(
+						(subQuestion) => {
 							// The subQuestions array is nested inside the SubQuestions property
 							const subQuestionsData = subQuestion.SubQuestions;
-		
+
 							// Process each subquestion in the subQuestionsData array
 							const types = subQuestionsData.map((type) => {
 								// Extract the miniQuestions for each subquestion type
-								const miniQuestions = type.subQuestions.map((miniQuestion) => ({
-									question: miniQuestion.question,
-									hint: miniQuestion.hint,
-								}));
-		
+								const miniQuestions = type.subQuestions.map(
+									(miniQuestion) => ({
+										question: miniQuestion.question,
+										hint: miniQuestion.hint,
+									}),
+								);
+
 								return {
 									label: type.tag,
 									id: type._id,
@@ -302,113 +302,118 @@ const Form = () => {
 									miniQuestions: miniQuestions,
 								};
 							});
-		
+
 							return {
 								label: subQuestion.tag,
 								question: subQuestion.question,
 								types: types,
 							};
-						});
-		
-						setSubquestions(mappedSubQuestions);
-					}
-				}
-			} catch (error) {
-				console.error('Error fetching form data:', error);
-			}
-		};
-		
-		useEffect(() => {
-			// Fetch data from the teacher API and populate the form on mount
-			fetchFormData();
-		}, []);
-	
+						},
+					);
 
-		const handleSaveInfoCenter = () => {
-			if (mainQuestion.trim() === '' || (image === null && pdf === null)) {
-				setShowPopup(true);
-			} else {
-				const formData = new FormData();
-				formData.append('questionText', mainQuestion);
-		
-				if (image) {
-					formData.append('image', image);
-					if (!imageURL.startsWith('media/images/')) {
-						formData.append('imagePath', 'media/images/' + image.name);
-					}
-				} else {
-					// If image is not selected, send the existing image path again
-					formData.append('imagePath', imageURL);
-				}
-		
-				if (pdf) {
-					formData.append('info', pdf);
-				} else {
-					// If pdf is not selected, send the existing pdf path again
-					formData.append('infoPath', pdfURL);
-				}
-		
-				if (questionId) {
-					// If questionId is present in the URL, edit the existing question
-					fetch(`http://localhost:5000/question/edit/main?questionId=${questionId}`, {
-						method: 'POST',
-						body: formData,
-						headers: {
-							Authorization: `Bearer ${localStorage.getItem('token')}`,
-						},
-						
-					})
-						.then((response) => {
-							if (response.ok) {
-								console.log('Main question edited successfully!');
-								// Handle success and any other logic here...
-							} else {
-								throw new Error('Error editing main question');
-							}
-						})
-						.catch((error) => {
-							console.error('Error editing main question:', error);
-						});
-						console.log(formData);
-				} else {
-					// If questionId is not present in the URL, create a new question
-					fetch('http://localhost:5000/question/create/main', {
-						method: 'POST',
-						body: formData,
-						headers: {
-							Authorization: `Bearer ${localStorage.getItem('token')}`,
-						},
-					})
-						.then((response) => {
-							if (response.ok) {
-								return response.json();
-							} else {
-								throw new Error('Error saving main question');
-							}
-						})
-						.then((data) => {
-							const questionId = data.questionId;
-							console.log('Main question saved successfully!');
-							console.log('Question ID:', questionId);
-							// Save the question ID in the state
-							setQuestionId(questionId);
-		
-							// Redirect to the dynamic route with the question ID
-							navigate(`/question/${questionId}`);
-		
-							// Perform any actions after successfully saving the main question
-							// Now you can use the questionId to associate the subquestions with the main question
-		
-							// Call the function to save subquestions with the retrieved questionId
-							// Pass the questionId here
-						})
-						.catch((error) => {
-							console.error('Error saving main question:', error);
-						});
+					setSubquestions(mappedSubQuestions);
 				}
 			}
-		};
-		
+		} catch (error) {
+			console.error('Error fetching form data:', error);
+		}
+	};
+
+	useEffect(() => {
+		// Fetch data from the teacher API and populate the form on mount
+		fetchFormData();
+	}, []);
+
+	const handleSaveInfoCenter = () => {
+		if (mainQuestion.trim() === '' || (image === null && pdf === null)) {
+			setShowPopup(true);
+		} else {
+			const formData = new FormData();
+			formData.append('questionText', mainQuestion);
+
+			if (image) {
+				formData.append('image', image);
+				if (!imageURL.startsWith('media/images/')) {
+					formData.append('imagePath', 'media/images/' + image.name);
+				}
+			} else {
+				// If image is not selected, send the existing image path again
+				formData.append('imagePath', imageURL);
+			}
+
+			if (pdf) {
+				formData.append('info', pdf);
+			} else {
+				// If pdf is not selected, send the existing pdf path again
+				formData.append('infoPath', pdfURL);
+			}
+
+			if (questionId) {
+				// If questionId is present in the URL, edit the existing question
+				fetch(
+					`http://localhost:5000/question/edit/main?questionId=${questionId}`,
+					{
+						method: 'POST',
+						body: formData,
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem(
+								'token',
+							)}`,
+						},
+					},
+				)
+					.then((response) => {
+						if (response.ok) {
+							console.log('Main question edited successfully!');
+							// Handle success and any other logic here...
+						} else {
+							throw new Error('Error editing main question');
+						}
+					})
+					.catch((error) => {
+						console.error('Error editing main question:', error);
+					});
+				console.log(formData);
+			} else {
+				// If questionId is not present in the URL, create a new question
+				fetch('http://localhost:5000/question/create/main', {
+					method: 'POST',
+					body: formData,
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem(
+							'token',
+						)}`,
+					},
+				})
+					.then((response) => {
+						if (response.ok) {
+							return response.json();
+						} else {
+							throw new Error('Error saving main question');
+						}
+					})
+					.then((data) => {
+						const questionId = data.questionId;
+						console.log('Main question saved successfully!');
+						console.log('Question ID:', questionId);
+						// Save the question ID in the state
+						setQuestionId(questionId);
+
+						// Redirect to the dynamic route with the question ID
+						navigate(`/question/${questionId}`);
+
+						// Perform any actions after successfully saving the main question
+						// Now you can use the questionId to associate the subquestions with the main question
+
+						// Call the function to save subquestions with the retrieved questionId
+						// Pass the questionId here
+					})
+					.catch((error) => {
+						console.error('Error saving main question:', error);
+					});
+			}
+		}
+	};
 
 	const handleSave = (questionIndex) => {
 		const subquestion = subquestions[questionIndex];
@@ -597,34 +602,33 @@ const Form = () => {
 							type="text"
 							id="main-question"
 							value={mainQuestion}
-						onChange={(e) => setMainQuestion(e.target.value)}
-
+							onChange={(e) => setMainQuestion(e.target.value)}
 						/>
 					</div>
 					<div className="form-group image-container">
-        <label htmlFor="image" className="form-label">
-          Image:
-        </label>
-        <input
-          className="form-input"
-          type="file"
-          id="image"
-          accept="image/*"
-          onChange={handleImageChange}
-        />
-      </div>
+						<label htmlFor="image" className="form-label">
+							Image:
+						</label>
+						<input
+							className="form-input"
+							type="file"
+							id="image"
+							accept="image/*"
+							onChange={handleImageChange}
+						/>
+					</div>
 
-      {/* Display the selected image */}
-      {selectedImageURL && (
-        <div className="selected-image-container">
-          <span className="form-label">Selected Image:</span>
-          <img
-            className="selected-image"
-            src={selectedImageURL}
-            alt="Selected"
-          />
-        </div>
-      )}
+					{/* Display the selected image */}
+					{selectedImageURL && (
+						<div className="selected-image-container">
+							<span className="form-label">Selected Image:</span>
+							<img
+								className="selected-image"
+								src={selectedImageURL}
+								alt="Selected"
+							/>
+						</div>
+					)}
 					<div className="form-group pdf-container">
 						<label htmlFor="pdf" className="form-label">
 							PDF File (Info Centre):
@@ -649,78 +653,119 @@ const Form = () => {
 						</button>
 					</div>
 					{subquestions.map((subquestion, subquestionIndex) => (
-            <div className="subquestion" key={subquestionIndex}>
-              <div className="form-group">
-                <label className="subquestion-label">
-                  {subquestion.label}:
-                </label>
-                <input
-                  className="form-input"
-                  type="text"
-                  value={subquestion.question}
-                  onChange={(e) =>
-                    handleSubquestionChange(subquestionIndex, e)
-                  }
-                />
-              </div>
-              {subquestion.types.map((type, typeIndex) => (
-                <div className="form-group" key={typeIndex}>
-                  {type.label !== "Calculation" && (
-                    <div className="dropdown-container">
-                      <label className="dropdown-label">{type.label}:</label>
-                      <select
-                        className="dropdown wide-dropdown"
-                        value={type.numMiniQuestions}
-                        onChange={(e) =>
-                          handleNumMiniQuestionsChange(
-                            subquestionIndex,
-                            typeIndex,
-                            e
-                          )
-                        }
-                      >
-                        {[1, 2, 3, 4, 5].map((num) => (
-                          <option key={num} value={num}>
-                            {num}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
-                  )}
-                  {type.numMiniQuestions > 0 && (
-                    <div className="mini-questions-container">
-                      {type.miniQuestions.map((miniQuestion, miniQuestionIndex) => (
-                        <div className="form-group mini-question" key={miniQuestionIndex}>
-                          <label className="form-label">Mini-Question {miniQuestionIndex + 1}:</label>
-                          <input
-                            className="form-input"
-                            type="text"
-                            value={miniQuestion.question}
-                            onChange={(e) => handleMiniQuestionChange(subquestionIndex, typeIndex, miniQuestionIndex, e)}
-                          />
-                          <label className="form-label">Hint:</label>
-                          <input
-                            className="form-input fixed-height"
-                            value={miniQuestion.hint}
-                            onChange={(e) => handleHintChange(subquestionIndex, typeIndex, miniQuestionIndex, e)}
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              ))}
-              <div className="form-group">
-                <button
-                  type="button"
-                  className="save-button"
-                  onClick={() => handleSave(subquestionIndex)}
-                >
-                  Save
-                </button>
-              </div>
-            </div>
-          ))}
+						<div className="subquestion" key={subquestionIndex}>
+							<div className="form-group">
+								<label className="subquestion-label">
+									{subquestion.label}:
+								</label>
+								<input
+									className="form-input"
+									type="text"
+									value={subquestion.question}
+									onChange={(e) =>
+										handleSubquestionChange(
+											subquestionIndex,
+											e,
+										)
+									}
+								/>
+							</div>
+							{subquestion.types.map((type, typeIndex) => (
+								<div className="form-group" key={typeIndex}>
+									{type.label !== 'Calculation' && (
+										<div className="dropdown-container">
+											<label className="dropdown-label">
+												{type.label}:
+											</label>
+											<select
+												className="dropdown wide-dropdown"
+												value={type.numMiniQuestions}
+												onChange={(e) =>
+													handleNumMiniQuestionsChange(
+														subquestionIndex,
+														typeIndex,
+														e,
+													)
+												}
+											>
+												{[1, 2, 3, 4, 5].map((num) => (
+													<option
+														key={num}
+														value={num}
+													>
+														{num}
+													</option>
+												))}
+											</select>
+										</div>
+									)}
+									{type.numMiniQuestions > 0 && (
+										<div className="mini-questions-container">
+											{type.miniQuestions.map(
+												(
+													miniQuestion,
+													miniQuestionIndex,
+												) => (
+													<div
+														className="form-group mini-question"
+														key={miniQuestionIndex}
+													>
+														<label className="form-label">
+															Mini-Question{' '}
+															{miniQuestionIndex +
+																1}
+															:
+														</label>
+														<input
+															className="form-input"
+															type="text"
+															value={
+																miniQuestion.question
+															}
+															onChange={(e) =>
+																handleMiniQuestionChange(
+																	subquestionIndex,
+																	typeIndex,
+																	miniQuestionIndex,
+																	e,
+																)
+															}
+														/>
+														<label className="form-label">
+															Hint:
+														</label>
+														<input
+															className="form-input fixed-height"
+															value={
+																miniQuestion.hint
+															}
+															onChange={(e) =>
+																handleHintChange(
+																	subquestionIndex,
+																	typeIndex,
+																	miniQuestionIndex,
+																	e,
+																)
+															}
+														/>
+													</div>
+												),
+											)}
+										</div>
+									)}
+								</div>
+							))}
+							<div className="form-group">
+								<button
+									type="button"
+									className="save-button"
+									onClick={() => handleSave(subquestionIndex)}
+								>
+									Save
+								</button>
+							</div>
+						</div>
+					))}
 					<div className="form-group">
 						<button
 							type="button"
